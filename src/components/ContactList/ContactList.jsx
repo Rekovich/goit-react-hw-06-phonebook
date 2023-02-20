54dacc7b724a0ae4730dabc80cdcfd4620cccffb
Rostyslav Kachor <rostynia@gmail.com>
2023-02-20 21:20:00 +0200

    fix deploy

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,45 +1,30 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import ContactItem from "components/ContactItem/ContactItem";
-import { useDispatch, useSelector } from "react-redux";
-import { selectContacts } from "redux/contacts/contacts-selector";
-import { deleteContactAction } from "redux/contacts/contacts-slice";
-import { selectFilteredContacts } from "redux/filter/filter-selector";
-
+import ContactItem from 'components/ContactItem/ContactItem';
+import { useSelector } from 'react-redux';
+import { selectFilteredContacts } from 'redux/filter/filter-selector';
 
 const ContactList = () => {
-  const dispatch = useDispatch();
-
-  const deleteContact = (id)=>  dispatch(deleteContactAction(id))
-  
-
-    const contacts = useSelector(selectFilteredContacts)
+  const contacts = useSelector(selectFilteredContacts);
   return (
-      <ul>{
-          contacts.map(({ id, name, number }) => {
-              return (
-                  <ContactItem
-                  key={id}
-                  name={name}
-                  number={number}
-                  contactId={id}
-                  // onDeleteContact={onDeleteContact}
-                  />
-              );
-          })}   
-      </ul>
-  )
+    <ul>
+      {contacts.map(({ id, name, number }) => {
+        return (
+          <ContactItem key={id} name={name} number={number} contactId={id} />
+        );
+      })}
+    </ul>
+  );
 };
 
 ContactList.propTypes = {
-contacts: PropTypes.arrayOf(
-  PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-  })
-),
-// onDeleteContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
 };
 
 export default ContactList;
